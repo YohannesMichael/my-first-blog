69f0d1c3961fae655238f1423eae0f234494b40e
cloud444 <y_michael@outlook.com>
2024-03-25 00:03:48 +1100

    created input values variables and check for empty input field

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,16 @@
 document.getElementById('submit').addEventListener('click', function(event) {
-    
+    event.preventDefault();
+
+    // Grab input values
+    const username = document.getElementById('username').value;
+    const blogTitle = document.getElementById('title').value;
+    const content = document.getElementById('title').value;
+
+    // No empty input check
+    if (!username || !blogTitle || !content) {
+        alert('Mate you gotta fill out all the fields.');
+        return;
+    }
 })
 
 
